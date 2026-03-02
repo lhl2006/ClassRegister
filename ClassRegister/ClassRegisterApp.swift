0c5eedcd893e6e06b8a26f4f91b5f00e7c3aadaa
lhl2006 <2081917073@qq.com>
2026-03-02 18:35:37 +0800

    Initial Commit

---
+++ ClassRegister/ClassRegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassRegisterApp.swift
+//  ClassRegister
+//
+//  Created by lhl on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct ClassRegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
